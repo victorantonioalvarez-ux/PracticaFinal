d9635e81784e3bb19ebef2cfb9a5dc943785f089
Víctor <victorantonioalvarez@paucasesnovescifp.cat>
2026-04-29 11:28:58 +0200

    feat: añadida recogida de datos
    <Descripcion>
    Recoge los datos tanto del nombre y el color de la categoria y las envia a la funcion de models.js

---
+++ js/categorias.js
@@ -0,0 +1,13 @@
+import Category from "models.js"
+
+document.addEventListener("DOMContentLoaded",function(){
+
+    const category = document.getElementById("form-categorias");
+
+    category.addEventListener("submit", function(event){
+        event.preventDefault();
+
+        const name = getElementById("nueva-categ");
+        const color = getElementById("color-categ");
+    });
+});
