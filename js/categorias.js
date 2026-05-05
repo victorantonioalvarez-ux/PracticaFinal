11f0be7502ad95f7a34188bb13a708b4441580cc
Victor <victorantonioalvarez@paucasesnovescifp.cat>
2026-05-05 18:04:10 +0200

    refactor: mejorada la recogida de datos en la pagina de categorias.

--- js/categorias.js
+++ js/categorias.js
@@ -1,13 +1,27 @@
-import Category from "models.js"
+import Category from "modelos.js"
 
 document.addEventListener("DOMContentLoaded",function(){
+    
+    mostrarCategorias();
 
-    const category = document.getElementById("form-categorias");
+    const formCategory = document.getElementById("form-categorias");
 
     category.addEventListener("submit", function(event){
         event.preventDefault();
 
-        const name = getElementById("nueva-categ");
-        const color = getElementById("color-categ");
+        const name = getElementById("nueva-categoria");
+        const color = getElementById("color-categoria");
+
+        if (nombre === ""){
+            alert("El nombre de la categoria no puede estar vacio.");
+            return;
+        }
+
     });
+
+    const nuevacategoria = crearCategoria(nombre, color);
+    agregarCategoria(nuevaCategoria);
+
+    formCategory.requestFullscreen();
+    mostrarCategorias();
 });
