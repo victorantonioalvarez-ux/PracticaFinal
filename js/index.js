9f796c9d8b67f82bc8ba37b48403105033e9815f
Victor <victorantonioalvarez@paucasesnovescifp.cat>
2026-05-10 20:39:45 +0200

    feat: funcion para incertar las tarjetas de tareas

--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@ function mostrarTareas(){
     const tareas = obtenerTareas();
 
     const pendientes = tareas.filter(function(t){
-        return t.realizada === false;
+        return t.terminada === false;
     });
 
     const terminadas = tareas.filter(function(t){
-        return t.realizada === true;
+        return t.terminada === true;
     });
 
     const listaPendientes = docuemnt.querySelectorAll(".lista-tareas")[0];
@@ -30,4 +30,33 @@ function mostrarTareas(){
             listaTerminadas.appendChild(crearTarjetaTarea(tarea));
         });
     }
+}
+
+function crearTarjetaTarea(tarea) {
+    const div = document.createElement("div");
+    div.classList.add("tarjeta-tarea")
+
+    if(tarea.terminada){
+        div.classList.add("tarea-terminada")
+    }
+
+    div.innerHTML =
+            `<div class="tarea-info">
+            <h3 class="tarea-titulo">${tarea.titulo}</h3>
+            <p class="tarea-descripcion">${tarea.descripcion}</p>
+            <div class="tarea-meta">
+                <span class="tarea-fecha"> ${tarea.fecha}</span>
+                <span class="tarea-categoria"> ${tarea.categoria}</span>
+                <span class="tarea-prioridad"> ${tarea.prioridad}</span>
+            </div>
+        </div>
+        <div class="tarea-acciones">
+            <button class="btn-completar" onclick="toggleTerminada('${tarea.id}')">
+                ${tarea.terminada ? "↩ Desmarcar" : "✓ Completar"}
+            </button>
+            <button class="btn-eliminar" onclick="eliminarTareaYActualizar('${tarea.id}')">
+                🗑️ Eliminar
+            </button>
+        </div>`;
+    return div;
 }
